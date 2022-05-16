165f5b08ae7b7adc956c189432513fd3fc509d1f
sinmetal <sinmetal@sinmetalcraft.jp>
2022-05-16 13:23:09 +0900

    orderingするときはEnableMessageOrdering=trueを設定
    `Topic.EnableMessageOrdering=false, but an OrderingKey was set in Message. Please remove the OrderingKey or turn on Topic.EnableMessageOrdering` と言われるので

--- pubsub_service.go
+++ pubsub_service.go
@@ -21,7 +21,11 @@ func (s *PubSubService) Publish(ctx context.Context, projectID string, topicID s
 	ctx = trace.StartSpan(ctx, "PubSubService/Publish")
 	defer trace.EndSpan(ctx, err)
 
-	ret := s.ps.TopicInProject(topicID, projectID).Publish(ctx, msg)
+	topic := s.ps.TopicInProject(topicID, projectID)
+	if msg.OrderingKey != "" {
+		topic.EnableMessageOrdering = true
+	}
+	ret := topic.Publish(ctx, msg)
 	serverID, err = ret.Get(ctx)
 	if err != nil {
 		return "", err
